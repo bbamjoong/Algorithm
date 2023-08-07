16744a9e1a9796c1b24be00417eb213613df0869
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 16:18:03 +0900

    [Silver II] Title: 외판원 순회 2, Time: 140 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,30 +1,37 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-cost = [list(map(int, input().split())) for i in range(n)]
-ans = sys.maxsize
-visited = [0] * n
-
 def dfs(start, now, value, cnt):
     global ans
-    if cnt==n:
-        if cost[now][start] != 0:
-            value += cost[now][start]
+
+    # 모든 곳을 방문했을 때
+    if cnt == n:
+        # 마지막 섬에서 처음 섬으로 이동할 수 있다면
+        if a[now][start] != 0:
+            value += a[now][start]
             if ans > value:
                 ans = value
-            return
+        return
 
+    # 지금까지 구한 ans보다 value가 크다면 return
     if value > ans:
         return
 
     for i in range(n):
-        if visited[i] == 0 and cost[now][i] != 0:
+        if not visited[i] and a[now][i]:
             visited[i] = 1
-            dfs(start, i, value+cost[now][i], cnt+1)
-            visited[i] = 0    
+            dfs(start, i, value + a[now][i], cnt + 1)
+            visited[i] = 0
+
+
+n = int(input())
+a = [list(map(int, input().split()))for _ in range(n)]
+visited = [0] * n
+
+ans = sys.maxsize
 
 for i in range(n):
     visited[i] = 1
-    dfs(i,i,0,1)
+    dfs(i, i, 0, 1)
     visited[i] = 0
+    
 print(ans)
