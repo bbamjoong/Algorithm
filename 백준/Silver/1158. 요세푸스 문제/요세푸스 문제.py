ea600a434366fdd58af18d06bf688040bfa53855
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 11:30:41 +0900

    Update 요세푸스 문제.py

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,14 +1,11 @@
-N,K=map(int,input().split())
-a=[i for i in range(1,N+1)]
-num=0
-print("<",end="")
-for i in range(N):
-    num+=K-1
-    if(num>=len(a)):
-        num=num%len(a)
-    if(i==N-1):
-        print(a[num],end="")
-    else:
-        print(a[num],end=", ")
-    del a[num]
-print(">",end="")
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque(range(1,n+1))
+res = []
+
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())
+    res.append(q.popleft())
+print(f'<{", ".join(map(str, res))}>')
