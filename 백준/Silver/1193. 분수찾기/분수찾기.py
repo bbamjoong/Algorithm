1955bc3d3ee78824b0b01b5395dfc88108cde009
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 11:36:29 +0900

    [Silver V] Title: 분수찾기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+a = 1
+while True:
+  x = a*(a+1)/2
+  y = a*(a+1)/2 -(a-1)
+  if n > x or n < y:
+    a+=1
+  else:
+    break
+k = a*(a-1)/2
+if a % 2 == 0:
+  ja = n-k
+  mo = a+1 - ja
+else:
+  mo = n-k
+  ja = a+1 - mo
+
+print(int(ja), end='')
+print('/', end='')
+print(int(mo))
