9ea91a7e6a1568213732a8564f5d162c3b622544
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 18:31:27 +0900

    Update 4연산.py

--- 백준/Gold/14395. 4연산/4연산.py
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -13,6 +13,7 @@ def bfs():
     res = -1
     q = deque()
     q.append((s,''))
+    # 한번 도달한 숫자는 다시 만들 필요가 없다. 연산만 더 할 뿐이기 때문이다.
     check = []
     check.append(s)
 
@@ -22,6 +23,7 @@ def bfs():
             res = ans
             break
 
+        # -를 하게되면 0이 되기 때문에 고려할 필요가 없다.
         for i in ('*', '+', '/'):
             if i == '*':
                 a = num * num
@@ -30,6 +32,8 @@ def bfs():
             else:
                 a = 1
 
+            # 나누기를 하면 1이된다. 즉, 답안보다 숫자가 클 때 숫자를 줄이는 방법이 없다는 것이다.
+            # 따라서 숫자가 0이상 t이하여야 하고, 도달하지 않았던 숫자일 때 조건이 만족한다.
             if 0 <= a <= t and a not in check:
                 check.append(a)
                 q.append((a,ans+i))
