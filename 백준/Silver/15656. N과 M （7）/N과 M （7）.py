4687561a3426a9ff8256ff164bd5b160a817ec73
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 18:59:18 +0900

    [Silver III] Title: N과 M (7), Time: 896 ms, Memory: 122456 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+res=[]
+
+def dfs():
+    if len(res) == m:
+        print(' '.join(map(str, res)))
+        return
+    
+    for i in nums:
+        res.append(i)
+        dfs()
+        res.pop()
+
+dfs()
