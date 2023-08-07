d22b543e56da2e0b71847c5570556409e2998c6a
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 15:50:57 +0900

    [Silver IV] Title: 국영수, Time: 416 ms, Memory: 61252 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for i in range(n):
+    a,b,c,d = map(str, input().split())
+    arr.append((a,int(b),int(c),int(d)))
+
+arr.sort(key = lambda x : (-x[1],x[2],-x[3],x[0]))
+
+for i in range(n):
+    print(arr[i][0])
