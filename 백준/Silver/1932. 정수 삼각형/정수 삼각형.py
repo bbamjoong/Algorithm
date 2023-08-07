54a13673ec389aa3e74fa450ac463eb64b09ae0e
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 14:03:57 +0900

    [Silver I] Title: 정수 삼각형, Time: 148 ms, Memory: 35616 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = []
+for i in range(n):
+    li.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    for j in range(len(li[i])):
+        if j == 0:
+            li[i][j] += li[i-1][j]
+        elif j == len(li[i])-1:
+            li[i][j] += li[i-1][j-1]
+        else:
+            li[i][j] += max(li[i-1][j-1], li[i-1][j])
+
+print(max(li[n-1]))
