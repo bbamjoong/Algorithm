b8513fa701b1f680bbfacbea3fda28cf167213e8
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 16:54:41 +0900

    Update 2048 （Easy）.py

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -7,89 +7,75 @@ board = [list(map(int, input().split())) for _ in range(n)]
 
 def up(graph):
     for j in range(n):
-        pointer = 0
+        maincheck = 0
         for i in range(1,n):
             if graph[i][j]:
-                tmp = graph[i][j]
-                graph[i][j] = 0
-
-                if graph[pointer][j] == 0:
-                    graph[pointer][j] = tmp
-                
-                elif graph[pointer][j] == tmp:
-                    graph[pointer][j] *= 2
-                    pointer += 1
-
+                #ex) 0 4 0 0 0 이라면 -> 4 0 0 0 0으로 바꿔준다.
+                if graph[maincheck][j] == 0:
+                    graph[maincheck][j], graph[i][j] = graph[i][j], 0
+                #ex) maincheck = 1, check = 3일 때 [0 2 0 2 0] -> [0 (2*2) 0 (2->0) 0]
+                #그리고 포인터를 밑으로 내려준다.                
+                elif graph[maincheck][j] == graph[i][j]:
+                    graph[maincheck][j], graph[i][j] = 2 * graph[maincheck][j], 0
+                    maincheck += 1
+                #ex) maincheck = 1, check = 3일 때 [0 2 0 4 0 ] -> [0 (2->4) 0 (4->2) 0]
+                #포인터 아래의 값과 check값을 바꿔준다.
                 else:
-                    pointer += 1
-                    graph[pointer][j] = tmp
+                    maincheck += 1
+                    graph[maincheck][j], graph[i][j] = graph[i][j], graph[maincheck][j]
     return graph
 
 def down(graph):
     for j in range(n):
-        pointer = n-1
+        maincheck = n-1
         for i in range(n-2,-1,-1):
             if graph[i][j]:
-                tmp = graph[i][j]
-                graph[i][j] = 0
-
-                if graph[pointer][j] == 0:
-                    graph[pointer][j] = tmp
-                
-                elif graph[pointer][j] == tmp:
-                    graph[pointer][j] *= 2
-                    pointer -= 1
-
+                if graph[maincheck][j] == 0:
+                    graph[maincheck][j], graph[i][j] = graph[i][j], 0                
+                elif graph[maincheck][j] == graph[i][j]:
+                    graph[maincheck][j], graph[i][j] = 2 * graph[maincheck][j], 0
+                    maincheck -= 1
                 else:
-                    pointer -= 1
-                    graph[pointer][j] = tmp
+                    maincheck -= 1
+                    graph[maincheck][j], graph[i][j] = graph[i][j], graph[maincheck][j]
     return graph
-            
+
 def left(graph):
     for i in range(n):
-        pointer = 0
+        maincheck = 0
         for j in range(1,n):
             if graph[i][j]:
-                tmp = graph[i][j]
-                graph[i][j] = 0
-
-                if graph[i][pointer] == 0:
-                    graph[i][pointer] = tmp
-
-                elif graph[i][pointer]  == tmp:
-                    graph[i][pointer] *= 2
-                    pointer += 1
-
+                if graph[i][maincheck] == 0:
+                    graph[i][maincheck], graph[i][j] = graph[i][j], 0                
+                elif graph[i][maincheck] == graph[i][j]:
+                    graph[i][maincheck], graph[i][j] = 2 * graph[i][maincheck], 0
+                    maincheck += 1
                 else:
-                    pointer += 1
-                    graph[i][pointer]= tmp                    
+                    maincheck += 1
+                    graph[i][maincheck], graph[i][j] = graph[i][j], graph[i][maincheck]
     return graph
 
 def right(graph):
     for i in range(n):
-        pointer = n - 1
-        for j in range(n - 2, -1, -1):
+        maincheck = n-1
+        for j in range(n-2,-1,-1):
             if graph[i][j]:
-                tmp = graph[i][j]
-                graph[i][j] = 0
-                if graph[i][pointer] == 0:
-                    graph[i][pointer] = tmp
-                elif graph[i][pointer]  == tmp:
-                    graph[i][pointer] *= 2
-                    pointer -= 1
+                if graph[i][maincheck] == 0:
+                    graph[i][maincheck], graph[i][j] = graph[i][j], 0                
+                elif graph[i][maincheck] == graph[i][j]:
+                    graph[i][maincheck], graph[i][j] = 2 * graph[i][maincheck], 0
+                    maincheck -= 1
                 else:
-                    pointer -= 1
-                    graph[i][pointer] = tmp
+                    maincheck -= 1
+                    graph[i][maincheck], graph[i][j] = graph[i][j], graph[i][maincheck]
     return graph
 
 ans = 0
 def dfs(graph, cnt):
     global ans
-
     if cnt == 5:
         ans = max(ans, max(map(max,graph)))
-        return
-    
+        return    
     dfs(up(deepcopy(graph)), cnt + 1)
     dfs(down(deepcopy(graph)), cnt + 1)
     dfs(left(deepcopy(graph)), cnt + 1)
