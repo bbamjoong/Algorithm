d9dc18cac1903bc7a5a553d0792dd340199be076
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 16:10:26 +0900

    Update 이전 순열.py

--- 백준/Silver/10973. 이전 순열/이전 순열.py
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -4,12 +4,16 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
+# ex)4 3 1 2
 for i in range(n-1,0,-1):
+    # i가 2일 때 arr[i-1] > arr[i] (3 > 1)
     if arr[i-1] > arr[i]:
         for j in range(n-1,0,-1):
+            # 배열을 거꾸로 탐색할 때 arr[i-1] > arr[j] (3 > 2)
             if arr[i-1] > arr[j]:
+                # 두 값을 swap / 4 3 1 2 -> 4 2 1 3
                 arr[i-1], arr[j] = arr[j], arr[i-1]
-                # print(arr[:i] + arr[i:].sort())
+                # i-1까지의 배열 + "정렬된"i~n-1까지의 배열
                 print(*(arr[:i] + sorted(arr[i:], reverse = True)))
                 exit()
 
