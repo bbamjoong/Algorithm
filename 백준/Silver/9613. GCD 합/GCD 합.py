e2652cbeaf95102662f8097d32cf6a51588b1f29
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 15:38:26 +0900

    Update GCD 합.py

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,16 +1,19 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations
+
 def gcd(a,b):
     while b>0:
         a,b = b,a%b
     return a
 
 t = int(input())
+
 for i in range(t):
     a, *li = map(int, input().split())
+
     sum_gcd = 0
-    combs = combinations(li,2)
-    for j in combs:
-        sum_gcd += gcd(j[0], j[1])
+    for i in range(len(li)-1):
+        for j in range(i+1, len(li)):
+            sum_gcd += gcd(li[i], li[j])
+
     print(sum_gcd)
