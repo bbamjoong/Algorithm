c0edad2f4835ee86d4620944783155b02a5b1d9e
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 11:37:21 +0900

    Update 분수찾기.py

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,14 +1,20 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 
 a = 1
+
 while True:
   x = a*(a+1)/2
   y = a*(a+1)/2 -(a-1)
+  
   if n > x or n < y:
     a+=1
   else:
     break
+    
 k = a*(a-1)/2
+
 if a % 2 == 0:
   ja = n-k
   mo = a+1 - ja
