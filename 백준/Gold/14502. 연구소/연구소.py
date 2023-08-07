e67b3a8e0af3d7b52361316f5680f6558e19ab09
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 18:49:58 +0900

    Update 연구소.py

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -14,6 +14,7 @@ def wall(cnt):
     if cnt == 3:
         bfs()
         return
+    # 벽을 둘 수 있는 모든 경우의 수를 check
     for i in range(m):
         for j in range(n):
             if graph[i][j] == 0:
@@ -28,6 +29,7 @@ def bfs():
 
     visited = [[0] * n for _ in range(m)]
     q = deque()
+    # 그래프를 깊은복사하여, 깊은복사를 한 그래프에 벽을 세우고 시뮬레이션
     graph1 = deepcopy(graph)
     for i in range(m):
         for j in range(n):
@@ -40,11 +42,15 @@ def bfs():
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
         
+            # 다음 좌표가 그래프 내에 있고 / 방문하지 않은 곳이고 / 빈 공간이어야 한다.
+            # 벽이 있을 때는 고려할 필요가 없다.
             if 0<=nx<m and 0<=ny<n and visited[nx][ny] == 0 and graph1[nx][ny] == 0:
                 visited[nx][ny] = 1
+                # 빈 공간을 바이러스로 최신화
                 graph1[nx][ny] = 2
                 q.append((nx,ny))
     
+    # 안전구역 카운팅
     bfs_cnt = 0
     for i in range(m):
         for j in range(n):
