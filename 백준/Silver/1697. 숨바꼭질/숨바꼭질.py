f63c58c9bf029b9a1d6dc21fe7362e8ee2d6dd23
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 12:21:30 +0900

    Update 숨바꼭질.py

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,23 +1,22 @@
 import sys
 input=sys.stdin.readline
 from collections import deque
-sys.setrecursionlimit(10**6)
+maxi = 100001
+s, d = map(int, input().split())
+visited = [0] * maxi
 
 def bfs():
-    q = deque()             
-    q.append(n)            
-    while  q:
-        x = q.popleft()     
-        if x == k:
-            print(dist[x])
+    q = deque()
+    q.append(s)
+    
+    while q:
+        x = q.popleft()
+        if x == d:
+            print(visited[x])
             break
-        for nx in (x - 1, x + 1, x * 2):    
-            if 0 <= nx <= MAX and not dist[nx]:
-                dist[nx] = dist[x] + 1
-                q.append(nx)    
-
-MAX = 10 ** 5              
-dist = [0] * (MAX + 1)      
-n, k = map(int, input().split())
+        for nx in (x-1, x+1, x*2):
+            if 0<=nx<maxi and visited[nx]==0:
+                visited[nx] = visited[x]+1
+                q.append(nx)
 
 bfs()
