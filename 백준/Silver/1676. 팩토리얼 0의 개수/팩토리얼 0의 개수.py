4143839412fc095115642b4c0f5201efd0fb0353
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 12:20:10 +0900

    Update 팩토리얼 0의 개수.py

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,3 +1,19 @@
 import sys
-n = int(input())
-print(n//5 + n//25 + n//125)
+input = sys.stdin.readline
+
+def fact(n):
+    if (n>1):
+        return n * fact(n-1)
+    else:
+        return 1
+    
+a = int(input())
+li = list(str(fact(a))[::-1])
+cnt = 0
+for i in range(len(li)):
+    if li[i] == '0':
+        cnt += 1
+    else:
+        break
+
+print(cnt)
