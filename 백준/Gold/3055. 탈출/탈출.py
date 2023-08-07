84acd9e9d0659541c73f8b92bf5e3d65b00a3d80
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-07 15:31:27 +0900

    [Gold IV] Title: 탈출, Time: 164 ms, Memory: 116372 KB -BaekjoonHub

---
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -0,0 +1,53 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+m, n = map(int, input().split())
+graph = [list(input().strip()) for _ in range(m)]
+visited = [[0] * n for _ in range(m)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+q = deque()
+
+
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == '*':
+            q.append((i,j))
+
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] == 'S':
+            q.append((i,j))
+        elif graph[i][j] == 'D':
+            Dx, Dy = i, j
+
+
+while q:
+    x, y = q.popleft()
+    if graph[Dx][Dy] == 'S':
+        print(visited[Dx][Dy])
+        exit()
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if nx<0 or nx>=m or ny<0 or ny>=n:
+            continue
+
+        # 고슴도치가 다음칸으로 이동할 때 . or D라면 / 이동이 가능하다.
+        if graph[x][y] == 'S' and (graph[nx][ny] == '.' or graph[nx][ny] == 'D'):
+            graph[nx][ny] = 'S'
+            visited[nx][ny] = visited[x][y] + 1
+            q.append((nx,ny))
+
+        # 물이 다음칸으로 이동할 때 . or S라면 / 이동이 가능하다.
+        # 물이 먼저 움직인다고 가정했을 때 물이 이동하는 방향이 S일 때는 고려안해도된다.
+        if graph[x][y] == '*' and (graph[nx][ny] == '.'):    
+            graph[nx][ny] = '*'
+            q.append((nx,ny))
+
+if not q:
+    print('KAKTUS')
