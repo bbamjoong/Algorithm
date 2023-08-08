50b2968b59f0005804e7f9281bdb90294ec548ca
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-08 10:08:50 +0900

    [Bronze II] Title: 시험 감독, Time: 408 ms, Memory: 221292 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -5,17 +5,13 @@ n = int(input())
 arr = list(map(int, input().split()))
 b, c = map(int, input().split())
 
-cnt = 0
+cnt = n
 for i in range(n):
     arr[i] -=b
-    if arr[i] < 0:
-        arr[i] = 0
-    cnt+=1
-
-for i in range(n):
-    if arr[i] % c:
-        cnt += arr[i] // c + 1
-    else:
-        cnt += arr[i] // c
+    if arr[i] > 0:
+        if arr[i] % c:
+            cnt += arr[i] // c + 1
+        else:
+            cnt += arr[i] // c
 
 print(cnt)
