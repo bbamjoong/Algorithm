c36aad81a5515b2766aff2b4ebbef8e1335f3f1b
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-08-25 19:33:42 +0900

    [Gold III] Title: 별자리 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -1,9 +1,14 @@
 import sys
 input = sys.stdin.readline
 
+# 최소 스패닝 트리를 이용한다. 간선이 k개일 때 시간복잡도 O(klogk). 간선의 개수는 100C2 = 100 * 99 / 2 = 4950.
+# 4950 * log(4950) = 4950 * 3.69 = 18,265
+
 n = int(input())
+# 별들의 좌표
 coor = [list(map(float, input().split())) for _ in range(n)]
 
+# 별1의 번호, 별2의 번호, 두 별 사이의 거리를 info에 추가한다.
 info = []
 for i in range(len(coor)):
     for j in range(i+1, len(coor)):
@@ -14,11 +19,13 @@ for i in range(len(coor)):
 
 parent = [0] + [x+1 for x in range(n)]
 
+# 부모 별 찾기
 def find(x):
     if x != parent[x]:
         return find(parent[x])
     return parent[x]
 
+# 두 별을 연결할 때 부모가 더 작은 별을 기준으로 부모 변경
 def union(a,b):
     a = find(a)
     b = find(b)
