86a09823b65af8a478e10b16fa5b9303911badbf
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-09-12 10:50:09 +0900

    [Gold III] Title: 치즈, Time: 272 ms, Memory: 120456 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -15,6 +15,7 @@ def bfs(x, y):
     visited = [[False] * m for _ in range(n)]
     visited[x][y] = True
 
+    # 치즈가 공기와 몇번 맞닿았는지 체크하는 리스트
     tmp =[[0] * m for _ in range(n)]
 
     while q:
@@ -25,15 +26,19 @@ def bfs(x, y):
 
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-
+            
+            # 방문하지 않은 곳
             if not visited[nx][ny]:
+                # 치즈라면? -> tmp += 1
                 if graph[nx][ny] == 1:
                     tmp[nx][ny] += 1
                 
+                # 공기라면? deque에 원소 추가
                 elif graph[nx][ny] == 0:
                     q.append([nx, ny])
                     visited[nx][ny] = True
 
+    # 치즈가 공기와 두번이상 맞닿았다면 제거
     for i in range(n):
         for j in range(m):
             if tmp[i][j] >= 2:
@@ -41,6 +46,9 @@ def bfs(x, y):
 
 ans = 0
 while True:
+
+    # 치즈가 놓이지 않은 곳에서부터 bfs시작
+    # (근데 가장자리에 치즈가 놓이지 않으니까 bfs(0,0)로 코드를 짜도 된다.)
     stop = False
     for i in range(n):
         if stop:
@@ -52,6 +60,7 @@ while True:
                 stop = True
                 break
     
+    # 모든칸이 0이라면 종료
     stop_while = True
     for i in range(n):
         for j in range(m):
