d17a4ddcd481afa858f852b460dbc17e3b0ef49a
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-09-19 09:48:45 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,28 +1,21 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-a, b = map(int, input().split())
+a,b = map(int,input().split())
 
-def bfs():
-    global a, b
-    q = deque()
-    q.append((a,1))
+cnt = 1
+while(b!=a):
+    cnt+=1
 
-    while q:
-        num, cnt = q.popleft()
-
-        if num == b:
-            return cnt
-            break
-
-        if num < b:
-            q.append((2*num, cnt+1))
-            q.appendleft((int(str(num)+'1'),cnt+1))
-
-ans = bfs()
-
-if ans:
-    print(ans)
+    tmp = b
+    
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
+    
+    if tmp == b:
+        print(-1)
+        break
 else:
-    print(-1)
+    print(cnt)
