c592ff8bea9df606a536aa1f04efb3f8b217eb32
bbamjoong <121084350+bbamjoong@users.noreply.github.com>
2023-10-09 11:04:04 +0900

    [Silver IV] Title: 스택 2, Time: 1340 ms, Memory: 71248 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stack = []
+
+for i in range(n):
+    a = input().split()
+
+    if int(a[0]) == 1:
+        stack.append(int(a[1]))
+
+    elif int(a[0]) == 2:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    
+    elif int(a[0]) == 3:
+        print(len(stack))
+
+    elif int(a[0]) == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+    
+    elif int(a[0]) == 5:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
