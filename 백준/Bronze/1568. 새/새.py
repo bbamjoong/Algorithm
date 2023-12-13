614978dc69092bb4a1b8645533f76fed629e81fa
HotFried <121084350+bbamjoong@users.noreply.github.com>
2023-12-14 07:23:42 +0900

    [Bronze II] Title: 새, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1568. 새/새.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt,i = 0,1
+
+while n>0:
+    if i > n:
+        i = 1
+        continue
+        
+    cnt += 1
+    n -= i
+    i+=1
+    
+print(cnt)
