fdeb61bbc9368650961a7b170ca7440e30b9bde6
HotFried <121084350+bbamjoong@users.noreply.github.com>
2023-12-24 13:50:11 +0900

    [Silver V] Title: 카드1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+nums = int(input())
+card = []
+ans = []
+
+for i in range(1, nums+1):
+    card.append(i)
+
+while (len(card) != 0):
+    ans.append(card.pop(0))
+    if(len(card) != 0):
+        card.append(card.pop(0))
+
+for j in ans:
+    print(j, end =" ")
