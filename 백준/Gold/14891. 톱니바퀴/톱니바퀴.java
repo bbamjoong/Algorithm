4f1e90f5d31ed546d49452f4971729aab30ab1d1
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-01-29 15:35:49 +0900

    [Gold V] Title: 톱니바퀴, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -25,14 +25,15 @@ public class Main {
         for (int t = 0; t < n; t++) {
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < 3; i++) {
-                if (Objects.equals(arr[i].get(2), arr[i + 1].get(6))) {
+                if (Objects.equals(arr[i].get(2), arr[i + 1].get(6))) { // 옆바퀴와 같은 극 = 1
                     same[i][1] = 1;
                     same[i + 1][0] = 1;
-                } else {
+                } else { // 옆바퀴와 다른 극 = -1
                     same[i][1] = -1;
                     same[i + 1][0] = -1;
                 }
             }
+
             int num = Integer.parseInt(st.nextToken()) - 1;
             int dir = Integer.parseInt(st.nextToken());
             change(num, dir, 0);
@@ -63,49 +64,55 @@ public class Main {
         if (num < 0 || num > 3) {
             return;
         }
+        /**
+         * vector = -1  -> 왼쪽으로 나아가기
+         * vector = 0   -> 처음 입력받아서 이동 방향이 없다
+         * vector = 1   -> 오른쪽으로 나아가기
+         */
+        if (vector == 0) { // 처음 입력받아 이동 방향이 없을 때
+            if (num == 0) { // 첫번째 톱니바퀴
+                rotate(arr[num], dir);
+                if (same[num][1] == 1) { // 두번째 톱니바퀴와 같은 극
+                    change(num + 1, 0, 1); // 두번째 톱니바퀴는 / 돌지 않는다. / 오른쪽으로 탐색
+                } else { // 두번째 톱니바퀴와 다른 극
+                    change(num + 1, -dir, 1); // 두번째 톱니바퀴는 / 첫번째 톱니바퀴와 다른 방향으로 돈다. / 오른쪽으로 탐색
+                }
+            } else if (num == 3) { // 네번째 톱니바퀴
+                rotate(arr[num], dir);
+                if (same[num][0] == 1) { // 세번째 톱니바퀴와 같은 극
+                    change(num - 1, 0, -1); // 세번째 톱니바퀴는 / 돌지 않는다. / 왼쪽으로 탐색
+                } else { // 세번째 톱니바퀴와 다른 극
+                    change(num - 1, -dir, -1); // 세번째 톱니바퀴는 / 네번째 톱니바퀴와 다른 방향으로 돈다. / 왼쪽으로 탐색
+                }
+            } else if (num == 1 || num == 2) { // 가운데 위치한 톱니바퀴
+                rotate(arr[num], dir);
+                // 왼쪽에 위치한 톱니바퀴 비교
+                if (same[num][0] == 1) { // 왼쪽 톱니바퀴가 같은 극
+                    change(num - 1, 0, -1); // 왼쪽 톱니바퀴는 / 돌지 않는다. / 왼쪽으로 탐색
+                } else { // 왼쪽 톱니바퀴가 다른 극
+                    change(num - 1, -dir, -1); // 왼쪽 톱니바퀴는 / 다른 방향으로 돈다. / 왼쪽으로 탐색
+                }
 
-        if (vector == 0) {
-            if (num == 0) {
-                rotate(arr[num], dir);
-                if (same[num][1] == 1) {
-                    change(num + 1, 0, 1);
-                } else {
-                    change(num + 1, -dir, 1);
-                }
-            } else if (num == 3) {
-                rotate(arr[num], dir);
-                if (same[num][0] == 1) {
-                    change(num - 1, 0, 1);
-                } else {
-                    change(num - 1, -dir, -1);
-                }
-            } else if (num == 1 || num == 2) {
-                rotate(arr[num], dir);
-                if (same[num][0] == 1) {
-                    change(num - 1, 0, -1);
-                } else {
-                    change(num - 1, -dir, -1);
-                }
-
-                if (same[num][1] == 1) {
-                    change(num + 1, 0, 1);
-                } else {
-                    change(num + 1, -dir, 1);
+                if (same[num][1] == 1) { // 오른쪽 톱니바퀴가 같은 극
+                    change(num + 1, 0, 1); // 오른쪽 톱니바퀴는 / 돌지 않는다. / 오른쪽으로 탐색
+                } else { // 오른쪽 톱니바퀴가 다른 극
+                    change(num + 1, -dir, 1); // 오른쪽 톱니바퀴는 / 다른 방향으로 돈다. / 오른쪽으로 탐색
                 }
             }
-        } else if (vector == 1) {
+        } else if (vector == 1) { // vector = 1 / 오른쪽 방향으로 나아가며 체크하는 경우
             rotate(arr[num], dir);
-            if (same[num][1] == 1) {
-                change(num + 1, 0, 1);
+            if (same[num][1] == 1) { // 오른쪽 톱니바퀴가 같은 극
+                change(num + 1, 0, 1); // 오른쪽 톱니바퀴는 / 돌지 않는다. / 오른쪽으로 탐색
             } else if (same[num][1] == -1) {
-                change(num + 1, -dir, 1);
+                change(num + 1, -dir, 1); // 오른쪽 톱니바퀴는 / 다른 방향으로 돈다. / 오른쪽으로 탐색
             }
-        } else if (vector == -1) {
+
+        } else if (vector == -1) { // vector = -1 / 왼쪽 방향으로 나아가며 체크하는 경우
             rotate(arr[num], dir);
-            if (same[num][0] == 1) {
-                change(num - 1, 0, -1);
+            if (same[num][0] == 1) { // 왼쪽 톱니바퀴가 같은 극
+                change(num - 1, 0, -1); // 왼쪽 톱니바퀴는 / 돌지 않는다. / 왼쪽으로 탐색
             } else if (same[num][0] == -1) {
-                change(num - 1, -dir, -1);
+                change(num - 1, -dir, -1); // 왼쪽 톱니바퀴는 / 다른 방향으로 돈다. / 왼쪽으로 탐색
             }
         }
     }
