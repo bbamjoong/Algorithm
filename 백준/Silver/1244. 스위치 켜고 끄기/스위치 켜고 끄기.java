9f3682baf69c43daa52ecc01d9b71158a1d882b2
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-01-30 15:06:12 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -7,75 +7,69 @@ public class Main {
     static int n;
     static int[] arr;
 
-    public static void main(String[] args) throws IOException {
-        //1 켜져있음. 0 꺼져있음.
-        // 남학생 : 스위치 번호가 자기가 받은 수의 "배수" -> 바꿈.
-        // 여학생 : 좌우대칭 탐색. 좌우가 같으면 index확장.
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        arr = new int[n + 1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int m = Integer.parseInt(br.readLine());
-        int a;
-        int b;
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
             if (a == 1) {
-                man(b - 1);
+                man(b, b);
             } else {
-                women(b - 1);
+                women(b, 0);
             }
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n + 1; i++) {
             sb.append(arr[i]).append(" ");
-            if ((i + 1) % 20 == 0) {
+            if (i % 20 == 0) {
                 sb.append("\n");
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
-    static void man(int num) {
-        for (int i = num; i < n; i += num + 1) {
-            change(i);
+    static void man(int num, int width) {
+        if (num > n) {
+            return;
         }
+
+        change(num);
+        num += width;
+        man(num, width);
     }
 
-    static void women(int num) {
-        int start = num - 1;
-        int end = num + 1;
+    static void women(int num, int width) {
+        if (num - width < 1 || num + width > n) {
+            return;
+        }
 
-        if (num == 0) {
-            change(num);
-        } else if (num == n - 1) {
+        if (width == 0) {
             change(num);
         } else {
-            while (start != 0 && end != n - 1) {
-                if (arr[start] == arr[end]) {
-                    start--;
-                    end++;
-                } else {
-                    break;
-                }
-            }
-
-            if (arr[start] != arr[end]) {
-                start++;
-                end--;
-            }
-
-            for (int i = start; i < end + 1; i++) {
-                change(i);
+            if (arr[num - width] == arr[num + width]) {
+                change(num - width);
+                change(num + width);
+            } else {
+                return;
             }
         }
+
+        women(num, width + 1);
     }
 
     static void change(int num) {
