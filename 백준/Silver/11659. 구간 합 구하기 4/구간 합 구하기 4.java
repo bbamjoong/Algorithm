5511c25a404e829f38d364d994f7804f60e03aa7
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-01-31 13:34:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 264 ms, Memory: 25804 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,63 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[] dp;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        dp = new int[n + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
-        }
-        
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            sb.append(dp[end] - dp[start - 1]).append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		
+		int N = nextInt();
+		int M = nextInt();
+		
+		int[] nums = new int[N];
+		nums[0] = nextInt();
+		for(int i=1; i<N; i++) {
+			nums[i] = nextInt() + nums[i-1];
+		}
+		
+		
+		for(int i=0; i<M; i++) {
+			int fromIdx = nextInt() - 1;
+			int toIdx = nextInt() - 1;
+			int sum = nums[toIdx];
+			if(fromIdx > 0) {
+				sum -= nums[fromIdx-1];
+			}
+			
+			sb.append(sum).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static final int BUFFER_SIZE = 1 << 13;
+	static byte[] buffer = new byte[BUFFER_SIZE];
+	static int bufferLen, bufferIdx;
+	
+	static int nextInt() throws IOException {
+		byte b;
+		int n = 0;
+		while((b=read()) <= 32);
+		do {
+			n = (n<<3) + (n<<1) + (b&15);
+		}while(isNumber(b=read()));
+		
+		return n;
+	}
+	
+	static boolean isNumber(byte b) {
+		return 47 < b && b < 58;
+	}
+	
+	static byte read() throws IOException {
+		if(bufferIdx == bufferLen) {
+			bufferLen = System.in.read(buffer, bufferIdx=0, BUFFER_SIZE);
+			if(bufferLen == -1) {
+				buffer[0] = -1;
+			}
+		}
+		return buffer[bufferIdx++];
+	}
 }
