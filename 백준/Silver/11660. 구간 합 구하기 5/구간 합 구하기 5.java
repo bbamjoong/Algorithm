03a0ca0886ed3a409f645f6c8419570db7c2e439
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-01-31 14:59:56 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 316 ms, Memory: 27336 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,36 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
-    static int n;
-    static int m;
     static int[][] arr;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = nextInt();
+        int m = nextInt();
 
         arr = new int[n + 1][n + 1];
         for (int i = 1; i < n + 1; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 1; j < n + 1; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+                arr[i][j] = nextInt() + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
             }
         }
-        
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
 
-            sb.append(arr[x2][y2] + arr[x1 - 1][y1 - 1] - arr[x2][y1 - 1] - arr[x1 - 1][y2]).append("\n");
+        for (int i = 0; i < m; i++) {
+            int x1 = nextInt() - 1;
+            int y1 = nextInt() - 1;
+            int x2 = nextInt();
+            int y2 = nextInt();
+
+            sb.append(arr[x2][y2] + arr[x1][y1] - arr[x2][y1] - arr[x1][y2]).append("\n");
         }
         System.out.println(sb);
+    }
+
+    static final int BUFFER_SIZE = 1 << 13;
+    static byte[] buffer = new byte[BUFFER_SIZE];
+    static int bufferLen, bufferIdx;
+
+    static int nextInt() throws IOException {
+        byte b;
+        int n = 0;
+        while ((b = read()) <= 32)
+            ;
+        do {
+            n = (n << 3) + (n << 1) + (b & 15);
+        } while (isNumber(b = read()));
+
+        return n;
+    }
+
+    static boolean isNumber(byte b) {
+        return 47 < b && b < 58;
+    }
+
+    static byte read() throws IOException {
+        if (bufferIdx == bufferLen) {
+            bufferLen = System.in.read(buffer, bufferIdx = 0, BUFFER_SIZE);
+            if (bufferLen == -1) {
+                buffer[0] = -1;
+            }
+        }
+        return buffer[bufferIdx++];
     }
 }
