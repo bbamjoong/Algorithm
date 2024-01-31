1003b251c1e838600ffc63575f3ae398d2c9da08
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-01-31 12:18:39 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,41 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 public class Main {
-    static StringBuilder sb = new StringBuilder();
 
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr[] = new int[9];
+		int sum = 0;
+		for(int i = 0;i<9; i++) {
+			arr[i] = (Integer.parseInt(br.readLine()));
+			sum += arr[i];
+		}
+		sum -= 100;
+		int ans1=0,ans2=0;
+		for(int i = 0;i<8;i++) {
+			for(int j = i+1; j<9; j++) {
+				if(arr[i] + arr[j] == sum) {
+					ans1 = i;
+					ans2 = j;
+					break;
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i<9; i++) {
+			if(i == ans1 || i == ans2)continue;
+			sb.append(arr[i]).append("\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
-        int sm = 0;
-        for (int i = 0; i < 9; i++) {
-            int a = Integer.parseInt(br.readLine());
-            arr[i] = a;
-            sm += a;
-        }
-
-        int idx1 = 0;
-        int idx2 = 0;
-
-        loop:
-        for (int i = 0; i < 9; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                if (sm - arr[i] - arr[j] == 100) {
-                    idx1 = i;
-                    idx2 = j;
-
-                    for (int k = 0; k < 9; k++) {
-                        if (k != idx1 && k != idx2) {
-                            sb.append(arr[k]).append("\n");
-                        }
-                    }
-                    break loop;
-                }
-            }
-        }
-
-        System.out.println(sb);
-
-    }
 }
