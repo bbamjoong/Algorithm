a550deae28d0ac5c8a331556857eaa75b8bcb733
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-01 11:04:40 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 240 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -9,7 +9,7 @@ public class Main {
     static int[][] arr;
     static ArrayList<Cloud> clouds = new ArrayList<>();
 
-    static boolean[][] visited;
+    static boolean[][] existClouds;
     static int ans;
 
     static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
@@ -29,25 +29,25 @@ public class Main {
             }
         }
 
-        setUp();
+        setInitialClouds(); // 구름 추가
 
         for (int i = 0; i < m; i++) {
-            visited = new boolean[n][n];
+            existClouds = new boolean[n][n];
             st = new StringTokenizer(br.readLine());
             int dir = Integer.parseInt(st.nextToken()) - 1;
             int cnt = Integer.parseInt(st.nextToken());
-            moveCloudAndAddWater(dir, cnt);
-            bugWater();
-            remakeCloud();
+            moveCloudAndAddWater(dir, cnt); // 구름 이동 + 물 추가
+            bugWater(); // 물복사 버그!
+            remakeCloud(); // 구름 재생성
         }
-        calculate();
+        calculate(); // 답 계산
 
         StringBuilder sb = new StringBuilder();
         sb.append(ans);
         System.out.println(sb);
     }
 
-    static void setUp() {
+    static void setInitialClouds() {
         clouds.add(new Cloud(n - 1, 0));
         clouds.add(new Cloud(n - 1, 1));
         clouds.add(new Cloud(n - 2, 0));
@@ -56,8 +56,11 @@ public class Main {
 
     static void moveCloudAndAddWater(int dir, int cnt) {
         for (Cloud cloud : clouds) {
+            // 모듈러 연산으로 n-1 초과 방지
             cloud.x = (cloud.x + dx[dir] * cnt) % n;
             cloud.y = (cloud.y + dy[dir] * cnt) % n;
+
+            // 좌표가 음수면 n 더해주기
             if (cloud.x < 0) {
                 cloud.x += n;
             }
@@ -65,8 +68,8 @@ public class Main {
                 cloud.y += n;
             }
 
-            arr[cloud.x][cloud.y]++;
-            visited[cloud.x][cloud.y] = true;
+            arr[cloud.x][cloud.y]++; // 물 추가
+            existClouds[cloud.x][cloud.y] = true; // 구름 존재여부 체크
         }
     }
 
@@ -74,32 +77,33 @@ public class Main {
         for (Cloud cloud : clouds) {
             int x = cloud.x;
             int y = cloud.y;
-            int cnt = 0;
-            for (int i = 1; i < 8; i += 2) {
+            int cnt = 0; // 물복사버그로 추가될 물의 양
+
+            for (int i = 1; i < 8; i += 2) { // 1, 3, 5, 7이 대각선입니다.
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
+                if (nx < 0 || ny < 0 || nx >= n || ny >= n) { // 범위 벗어나면 continue;
                     continue;
                 }
 
-                if (arr[nx][ny] > 0) {
+                if (arr[nx][ny] > 0) { // 물이 존재하면 cnt++
                     cnt++;
                 }
             }
-            arr[x][y] += cnt;
+            arr[x][y] += cnt; // 물복사버그!!
         }
     }
 
     static void remakeCloud() {
-        clouds.clear();
+        clouds.clear(); // 구름 ArrayList 초기화
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (visited[i][j] || arr[i][j] < 2) {
+                if (existClouds[i][j] || arr[i][j] < 2) { // 구름이 존재하거나 물이 2보다 적으면 continue
                     continue;
                 }
-                arr[i][j] -= 2;
-                clouds.add(new Cloud(i, j));
+                arr[i][j] -= 2; // 물의 양 2 감소
+                clouds.add(new Cloud(i, j)); // 새로 구름 추가
             }
         }
     }
