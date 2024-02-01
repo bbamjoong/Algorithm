e9155160becb21ff4582570eb58e7a717cb465cd
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-01 10:41:17 +0900

    [Gold IV] Title: 감시, Time: 156 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -11,8 +11,6 @@ public class Main {
     static int m;
     static int[][] arr;
     static ArrayList<CCTV> cctvList = new ArrayList<>();
-    static int wallX;
-    static int wallY;
     static int ans = (int) 1e9;
     static StringBuilder sb = new StringBuilder();
 
@@ -27,87 +25,101 @@ public class Main {
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                int a = Integer.parseInt(st.nextToken());
-                arr[i][j] = a;
+                int num = Integer.parseInt(st.nextToken());
+                arr[i][j] = num;
 
-                if (a >= 1 && a <= 5) {
-                    cctvList.add(new CCTV(i, j, a));
-                }
-                if (a == 6) {
-                    wallX = i;
-                    wallY = j;
+                if (num >= 1 && num <= 5) { // cctvList에 cctv 추가
+                    cctvList.add(new CCTV(i, j, num));
                 }
             }
         }
         dfs(0);
+
         sb.append(ans);
         System.out.println(sb);
-
     }
 
     static void dfs(int cnt) {
         if (cnt == cctvList.size()) {
-            int cntOfZero = calculate();
-            if (cntOfZero >= ans) {
-                return;
-            }
-            ans = Math.min(ans, cntOfZero);
+            calculate();
             return;
         }
 
         CCTV cctv = cctvList.get(cnt);
-        if (cctv.num == 1) {
-            for (int i = 0; i < 4; i++) {
-                go(cctv, i, false);
-                dfs(cnt + 1);
-                go(cctv, i, true);
-            }
-        } else if (cctv.num == 2) {
-            for (int i = 0; i < 2; i++) {
-                go(cctv, i, false);
-                go(cctv, i + 2, false);
-                dfs(cnt + 1);
-                go(cctv, i, true);
-                go(cctv, i + 2, true);
-            }
-        } else if (cctv.num == 3) {
-            for (int i = 0; i < 4; i++) {
-                go(cctv, i, false);
-                go(cctv, (i + 1) % 4, false);
-                dfs(cnt + 1);
-                go(cctv, i, true);
-                go(cctv, (i + 1) % 4, true);
-            }
-        } else if (cctv.num == 4) {
-            for (int i = 0; i < 4; i++) {
-                go(cctv, i, false);
-                go(cctv, (i + 3) % 4, false);
-                go(cctv, (i + 1) % 4, false);
-                dfs(cnt + 1);
-                go(cctv, i, true);
-                go(cctv, (i + 3) % 4, true);
-                go(cctv, (i + 1) % 4, true);
-            }
-        } else if (cctv.num == 5) {
-            go(cctv, 0, false);
-            go(cctv, 1, false);
-            go(cctv, 2, false);
-            go(cctv, 3, false);
-            dfs(cnt + 1);
-            go(cctv, 0, true);
-            go(cctv, 1, true);
-            go(cctv, 2, true);
-            go(cctv, 3, true);
-
+        switch (cctv.num) {
+            case (1):
+                startCCTV1(cctv, cnt);
+                break;
+            case (2):
+                startCCTV2(cctv, cnt);
+                break;
+            case (3):
+                startCCTV3(cctv, cnt);
+                break;
+            case (4):
+                startCCTV4(cctv, cnt);
+                break;
+            case (5):
+                startCCTV5(cctv, cnt);
         }
-
-
     }
 
+    static void startCCTV1(CCTV cctv, int cnt) {
+        for (int i = 0; i < 4; i++) {
+            go(cctv, i, false);
+            dfs(cnt + 1);
+            go(cctv, i, true);
+        }
+    }
 
+    static void startCCTV2(CCTV cctv, int cnt) {
+        for (int i = 0; i < 2; i++) {
+            go(cctv, i, false);
+            go(cctv, i + 2, false);
+            dfs(cnt + 1);
+            go(cctv, i, true);
+            go(cctv, i + 2, true);
+        }
+    }
+
+    private static void startCCTV3(CCTV cctv, int cnt) {
+        for (int i = 0; i < 4; i++) {
+            go(cctv, i, false);
+            go(cctv, (i + 1) % 4, false);
+            dfs(cnt + 1);
+            go(cctv, i, true);
+            go(cctv, (i + 1) % 4, true);
+        }
+    }
+
+    static void startCCTV4(CCTV cctv, int cnt) {
+        for (int i = 0; i < 4; i++) {
+            go(cctv, i, false);
+            go(cctv, (i + 3) % 4, false);
+            go(cctv, (i + 1) % 4, false);
+            dfs(cnt + 1);
+            go(cctv, i, true);
+            go(cctv, (i + 3) % 4, true);
+            go(cctv, (i + 1) % 4, true);
+        }
+    }
+
+    private static void startCCTV5(CCTV cctv, int cnt) {
+        for (int i = 0; i < 4; i++) {
+            go(cctv, i, false);
+        }
+        dfs(cnt + 1);
+        for (int i = 0; i < 4; i++) {
+            go(cctv, i, true);
+        }
+    }
+
+    // remove : true -> 백트래킹을 하면서 감시 구역을 다시 원래대로 돌리는 경우
+    // remove : false -> 감시구역 탐색
     static void go(CCTV cctv, int dir, boolean remove) {
         int nx = cctv.x;
         int ny = cctv.y;
+
         while (true) {
             nx += dx[dir];
             ny += dy[dir];
@@ -116,28 +128,29 @@ public class Main {
                 break;
             }
 
-            if (remove) {
-                if (arr[nx][ny] != 0) { // 백트래킹을 해야하는 경우
-                    arr[nx][ny]--;
-                    continue;
-                }
+            if (remove && arr[nx][ny] != 0) {  // 백트래킹을 해야하는 경우
+                arr[nx][ny]--;
+                continue;
             }
 
-            arr[nx][ny]++;
-
+            arr[nx][ny]++; // CCTV가 감시하는 구역이면 ++
         }
     }
 
-    static int calculate() {
+    static void calculate() {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 0) {
                     cnt++;
                 }
+
+                if (cnt >= ans) {
+                    return;
+                }
             }
         }
-        return cnt;
+        ans = Math.min(ans, cnt);
     }
 
     static class CCTV {
