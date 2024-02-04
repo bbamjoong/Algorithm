c1f80e01612f019337c68f6308740944172e6f27
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-05 00:04:21 +0900

    [Gold III] Title: 나무 재테크, Time: 164 ms, Memory: 15612 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -9,10 +9,66 @@ public class Main {
     static int k;
     static int[][] winterFood;
     static int[][] soil;
-    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+    static Tree[][] trees;
+    static int[] dx = {0, 0, -1, 1, 1, 1, -1, -1};
+    static int[] dy = {-1, 1, 0, 0, 1, -1, 1, -1};
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
+    static class Tree {
+        int cnt = 0;
+        int age = 0;
+        Tree before;
+        Tree next;
+
+        public Tree() { // 헤더 트리 만들기
+            before = next = this;
+        }
+
+        public Tree(int age) { // 문제에서 주어지는 트리 만들기
+            this.age = age;
+            cnt = 1;
+        }
+
+        public Tree(int age, int cnt) { // 번식할 트리 만들기(개수도 필요함)
+            this.age = age;
+            this.cnt = cnt;
+        }
+
+        // 문제에서 입력으로 주어지는 나무의 위치는 모두 다르다고 했다.
+        // 이후, 나무가 번식을 하면 새로운 나무를 추가해 주어야 하는데,
+        // 현재 Tree의 두번째 부분(첫번째는 편의상 빈 나무임 = 헤더)에 넣어주면 된다.
+
+        // A(헤더)의 뒤에 B를 넣으면 된다.
+        // A -> B -> C   =>    A -> D -> B -> C
+        public void add(Tree tree) {
+            this.next.before = tree;
+            tree.next = this.next;
+            tree.before = this;
+            this.next = tree;
+        }
+
+        // X.delete(Y)
+        // X 트리부터 Y트리 전까지 연결됨.
+
+        // A -> D -> C -> B 가 있다고 할 때
+        // A.delete(B) : A -> D
+        // A.delete(C) : A -> D -> B
+        // D.delete(B) : D -> C
+        // X == Y라면 자기 자신만 남음.
+        // X < Y라면 그대로.
+
+        // add를 하면 나이 순서대로 정렬이 될텐데
+        // 양분을 못 먹는 나무 이후로부터는 전부 죽어버린다.
+        // A -> B -> C -> D 가 있을때, C부터는 양분을 못먹어서 죽어버린다고 할 때
+        // A.delete(C)를 해주면 A -> B만 남게 되는 것이다.
+        public void delete(Tree tree) {
+            tree = tree.before;
+            tree.next = this;
+            this.before = tree;
+        }
+    }
+
+    static void setInputs() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,7 +78,8 @@ public class Main {
 
         winterFood = new int[n][n];
         soil = new int[n][n];
-        Tree[][] trees = new Tree[n][n];
+        trees = new Tree[n][n];
+
         for (int r = 0; r < n; r++) {
             st = new StringTokenizer(br.readLine());
             for (int c = 0; c < n; c++) {
@@ -40,148 +97,131 @@ public class Main {
 
             trees[x][y].add(new Tree(z));
         }
+    }
 
-        for (int k = 0; k < Main.k; k++) {
+    private static void performSpringAndSummerOperation() { // 봄 행동(여름을 곁들인)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
 
-            for (int i = 0; i < n; i++) { // 봄 여름
-                for (int j = 0; j < n; j++) {
+                boolean canAbsorbSoil = true;
+                Tree treeToDelete = null;
+                Tree tree = trees[i][j].next;
 
-                    boolean eat = true;
-                    Tree del = null;
-                    Tree tree = trees[i][j].next;
-
-                    while (tree.age > 0) {
-                        // 양분 섭취
-                        if (eat) {
-                            // 나무가 모두 양분을 섭취했을 때 필요한 양을 구함
-                            int sum = tree.cnt * tree.age;
-                            // 모든 나무가 양분을 먹을 수 있을 때
-                            if (soil[i][j] >= sum) {
-                                soil[i][j] -= sum;
-                                tree.age++;
-                            }
-                            // 일부만 양분을 먹을 수 있을 때
-                            else {
-                                // 양분을 먹을 수 있는 나무의 수
-                                int able = soil[i][j] / tree.age;
-                                // 죽을 나무의 수
-                                int deadCnt = tree.cnt - able;
-                                // 양분을 먹거나 양분이 되기
-                                soil[i][j] -= able * tree.age;
-                                soil[i][j] += deadCnt * (tree.age / 2);
-
-                                // 아직 살아있는 나무가 있는지
-                                if (able > 0) {
-                                    tree.cnt = able;
-                                    tree.age++;
-                                    del = tree.next;
-                                } else {
-                                    del = tree;
-                                }
-                                eat = false;
-                            }
+                while (tree.age > 0) { // 나무 끝까지 탐색해서 다시 헤더로 돌아 온다면 종료
+                    // 양분 섭취
+                    if (canAbsorbSoil) {
+                        int sum = tree.cnt * tree.age; // 해당 나무가 양분을 얼마나 섭취할 수 있니?
+                        if (soil[i][j] >= sum) { // 양분 섭취가 가능하다!
+                            soil[i][j] -= sum;
+                            tree.age++;
+                        } else { // 양분 섭취를 못해요!
+                            /**
+                             * 여름
+                             */
+                            treeToDelete = performSummerOperation(i, j, tree);
+                            canAbsorbSoil = false; // 더이상 양분을 먹을 나무는 없고 전부다 토양으로 돌아가요! 슬픕니다.
                         }
-                        // 양분이 되기(죽음)
-                        else {
-                            soil[i][j] += tree.cnt * (tree.age / 2);
-                        }
+                    } else { // 너흰 이제 토양으로 돌아가라.
+                        soil[i][j] += tree.cnt * (tree.age / 2);
+                    }
 
-                        tree = tree.next;
-                    }
-                    // 제거할 나무가 있고, 그게 헤더노드가 아닐 때
-                    if (del != null && del.age > 0) {
-                        trees[i][j].delete(del);
-                    }
+                    tree = tree.next; // 다음 트리를 탐색합니다.
                 }
+
+                // 모든 트리를 탐색했으니, 이제 제거할 트리가 있는지 찾아볼까요?
+                if (treeToDelete == null) {
+                    continue;
+                } // 제거할 트리가 있다면?? 제거해주면 됩니다.
+                trees[i][j].delete(treeToDelete);
             }
+        }
+    }
 
-            for (int i = 0; i < n; i++) { // 가을
-                for (int j = 0; j < n; j++) {
-                    Tree t = trees[i][j].next;
-                    while (t.age > 0) {
-                        // 나이가 5의 배수인 나무의 경우
-                        if (t.age % 5 == 0) {
-                            for (int dir = 0; dir < 8; dir++) {
-                                int nx = i + dx[dir];
-                                int ny = j + dy[dir];
+    static Tree performSummerOperation(int i, int j, Tree tree) { // 여름 행동
+        Tree treeToDelete;
+        int ableTreesCount = soil[i][j] / tree.age; // 현재 토양에서 지금 나이의 나무 몇그루가 양분을 섭취할 수 있나?
 
-                                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
-                                    continue;
-                                }
-                                // 이미 나이가 1인 나무 그룹이 생성되 있는지
-                                if (trees[nx][ny].next.age == 1) {
-                                    trees[nx][ny].next.cnt += t.cnt;
-                                } else {
-                                    trees[nx][ny].add(new Tree(1, t.cnt));
-                                }
+        soil[i][j] -= ableTreesCount * tree.age; // 양분을 먹었어요!
+        soil[i][j] +=
+                (tree.cnt - ableTreesCount) * (tree.age / 2); // 죽은 나무는 나이의 절반만큼 양분으로 돌아갑니다!
+
+        if (ableTreesCount > 0) { // 양분을 먹은 나무가 있었다면
+            tree.cnt = ableTreesCount; // tree의 cnt를 다시 갱신해준다.
+            tree.age++; // 한 살 더 먹으렴
+            treeToDelete = tree.next; // 이 다음 나무부터는 제거 해버린다.
+        } else { // 아무도 양분을 못먹었다면 이 나무부터 제거 해버린다.
+            treeToDelete = tree;
+        }
+        return treeToDelete;
+    }
+
+    private static void performAutumnOperation() { // 가을 행동
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Tree tree = trees[i][j].next;
+                while (tree.age > 0) { // 헤더로 다시 돌아오면 종료.
+                    // 나이가 5의 배수인 나무의 경우
+                    if (tree.age % 5 == 0) {
+                        for (int dir = 0; dir < 8; dir++) { // 8방 탐색 슛~~
+                            int nx = i + dx[dir];
+                            int ny = j + dy[dir];
+
+                            if (nx < 0 || ny < 0 || nx >= n || ny >= n) { // 맵 밖으로 벗어나면 continue
+                                continue;
                             }
-                        }
-                        t = t.next;
-                    }
-                }
-            }
 
-            for (int i = 0; i < n; i++) { // 겨울
-                for (int j = 0; j < n; j++) {
-                    soil[i][j] += winterFood[i][j];
+                            if (trees[nx][ny].next.age == 1) { // 이미 해당칸에 다른칸에서 번식해온 나무가 있으면
+                                // 헤더만 존재했다면 해더의 age는 0이라 조건을 만족 못해요.
+                                trees[nx][ny].next.cnt += tree.cnt; // 이제 번식할 나무의 개수만 더해주면 된다.
+                                continue;
+                            }
+
+                            trees[nx][ny].add(
+                                    new Tree(1, tree.cnt)); // 해당 칸에 처음 번식을 하는거라면 나이는 1, 번식할 나무의 개수로 tree 생성
+
+                        }
+                    }
+                    tree = tree.next; // 다음 트리 탐색
                 }
             }
         }
+    }
 
+    private static void performWinterOperation() { // 겨울 행동
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                soil[i][j] += winterFood[i][j];
+            }
+        }
+    }
+
+    static int calculateAnswer() { // 답 계산
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                Tree t = trees[i][j].next;
-                while (t.age > 0) {
-                    ans += t.cnt;
-                    t = t.next;
+                Tree tree = trees[i][j].next;
+                while (tree.age > 0) { // 헤더로 다시 돌아오면 종료
+                    ans += tree.cnt; // 나무 count 해주기
+                    tree = tree.next; // 다음 트리로 Go
                 }
             }
         }
-
-        System.out.println(ans);
+        return ans;
     }
 
-    static class Tree {
-        int cnt;
-        int age;
-        Tree before;
-        Tree next;
+    public static void main(String[] args) throws Exception {
+        setInputs();
 
-        // 처음 trees를 초기화할 때
-        public Tree() {
-            this.age = 0;
-            before = next = this;
+        for (int year = 1; year < k + 1; year++) { // 1년부터 K년 까지
+
+            performSpringAndSummerOperation();
+            performAutumnOperation();
+            performWinterOperation();
         }
 
-        public Tree(int age) {
-            this.age = age;
-            cnt = 1;
-        }
+        int ans = calculateAnswer();
 
-        public Tree(int age, int cnt) {
-            this.age = age;
-            this.cnt = cnt;
-        }
-
-        // This의 next에 tree를 넣는다.
-        // A -> B -> C   =>    A -> D -> B -> C
-        public void add(Tree tree) {
-            this.next.before = tree;
-            tree.next = this.next;
-            tree.before = this;
-            this.next = tree;
-        }
-
-        // This의 next노드를 없앤다.
-        // A -> D -> B -> C   =>    A -> B -> C
-        public void delete(Tree tree) {
-//            this.next = tree.before;
-//            tree.next.before = tree.before;
-            tree = tree.before;
-            tree.next = this;
-            this.before = tree;
-        }
+        sb.append(ans);
+        System.out.println(sb);
     }
-
 }
