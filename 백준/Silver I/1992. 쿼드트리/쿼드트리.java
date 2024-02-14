541bba88db6559afab095e3b94f74f9e0272abf8
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-14 17:09:27 +0900

    [Silver I] Title: 쿼드트리, Time: 84 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Silver I/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int n;
+    public static int[][] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String num = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = num.charAt(j) - '0';
+            }
+        }
+
+        QuadTree(0, 0, n);
+        System.out.println(sb);
+    }
+
+    public static void QuadTree(int x, int y, int size) {
+        if (isPossible(x, y, size)) {
+            sb.append(arr[x][y]);
+            return;
+        }
+
+        int newSize = size / 2;
+
+        sb.append('(');
+
+        QuadTree(x, y, newSize);
+        QuadTree(x, y + newSize, newSize);
+        QuadTree(x + newSize, y, newSize);
+        QuadTree(x + newSize, y + newSize, newSize);
+
+        sb.append(')');
+
+
+    }
+
+    static boolean isPossible(int x, int y, int size) {
+        int num = arr[x][y]; // 제일 구석 값
+
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (num != arr[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
