129f5e010baeac9c5448096e6b961ebaf56318c4
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-26 07:53:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 384 ms, Memory: 189804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,22 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-
-thing = [[0,0]]
-for _ in range(n):
-    thing.append(list(map(int, input().split())))
+thing = [list(map(int, input().split())) for _ in range(n)] # 물건[무게, 가치]
 
 dp = [[0] * (k+1) for _ in range(n+1)]
 
-for i in range(1,n+1):
-    for j in range(1,k+1):
-        weight = thing[i][0]
-        value = thing[i][1]
-
-        if j < weight: #가방에 못 넣는 경우:
-            dp[i][j] = dp[i-1][j]
-
-        else: #가방에 넣을 수 있는 경우:
+for i in range(1, n+1):
+    for j in range(1, k+1): # 무게
+        weight = thing[i-1][0]
+        value = thing[i-1][1]
+        
+        if j < weight:
+            dp[i][j] = dp[i-1][j] # 위칸의 무게 데려옴
+        
+        else: # 위칸의 무게 vs 내 무게를 뺀 위의 칸 + 내 가치
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value)
-
-print(dp[n][k])
+            
+print(dp[-1][-1])
