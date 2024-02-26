374c967e7a9275378bd057cc96514ca9fb7ce924
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-26 23:27:56 +0900

    [Gold III] Title: 공주님의 정원, Time: 392 ms, Memory: 124216 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -14,34 +14,26 @@ end = 301
 cnt = 0
 idx = 0
 
-while (idx < n):
-    # 정원의 마지막 꽃이 지는 날짜가 12월 1일 이상이 됐거나,
-    # 현재 확인할 꽃의 시작 날짜가 정원의 마지막 꽃이 지는 날짜와 이어지지 않을 경우, 탐색 종료
+while (idx < n): # 끝까지  조사
+    # 다음 꽃의 시작 날짜가 end보다 크면 -> 빈 날짜가 존재하므로 break
+    # end가 1201보다 크면 조기에 종료하면 된다.
     if (end >= 1201 or arr[idx][0] > end):
         break
 
     # 꽃이 피는 날짜가 end 이전일 때, 가장 느리게 지는 꽃의 날짜
-    temp_end_date = 0
-
+    tmpEnd = 0
     while (idx < n):
-        # 꽃이 피는 날짜가 end 이전이라면,
         if (arr[idx][0] <= end):
-            # 그 중 가장 느리게 지는 꽃의 날짜를 확인
-            if (temp_end_date <= arr[idx][1]):
-                temp_end_date = arr[idx][1]
-            idx += 1
+            tmpEnd = max(tmpEnd, arr[idx][1]) # 가장 느리게 피는것
+            idx += 1 # 다음 순회
         else:
-            idx -= 1
+            idx -= 1 # 없으면 idx - 1  => 뒤에서 idx + 1 해주니까 다른 조건으로 방문하게
             break
 
-    # 가장 꽃이 느리게 지는 날짜를 end_date로 수정
-    end = temp_end_date
-    # 심은 꽃의 개수 증가
+    end = tmpEnd
     cnt += 1
     idx += 1
 
-# 마지막으로 확인한 꽃의 지는 날짜가 12월 1일 보다 작으면, 
-# 3월 1일부터 11월 30일까지 계속 피어있는게 아니므로 0 출력
 if end < 1201:
     print(0)
 else:
