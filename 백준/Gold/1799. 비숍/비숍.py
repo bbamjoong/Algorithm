ffb101c86f4f3c19eb1dcebf505590b0faa18961
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-27 00:31:37 +0900

    [Gold I] Title: 비숍, Time: 1492 ms, Memory: 119220 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -8,29 +8,33 @@ diag_check1 = [0] * (2*n - 1)
 # 체스판의 \ 대각선 개수 : 2*n - 1
 diag_check2 = [0] * (2*n - 1)
 
-# 대각선에 놓을 수 있는 비숍의 개수를 미리 계산하는 함수
-def upper_bound(diag):
-    able_bishops = 0
-    for d in range(diag, 2*n - 1):
-        for y in range(d + 1):
-            x = d - y
-            if 0 <= x < n and 0 <= y < n and arr[x][y]:
-                able_bishops += 1
-                break
-    return able_bishops
+dp = [0] * (2*n - 1)
+if arr[n-1][n-1] == 1:
+    dp[-1] = 1
+
+for d in range(2*n-3, -1, -1):
+    for y in range(d+1):
+        x = d - y
+        if 0 <= x < n and 0 <= y < n and arr[x][y]:
+            dp[d] += dp[d+1] + 1
+            break
+
+    else:
+        dp[d] = dp[d+1]
+
+
 
 ans = 0
-
-# 백트래킹을 이용한 비숍 배치 함수
 def chess(diag, cnt):
     global ans
     if diag == 2*n - 1:
         ans = max(ans, cnt)
         return
 
-    # 대각선에 놓을 수 있는 비숍의 최대 개수를 미리 계산하여 탐색 범위를 줄임
-    ub = upper_bound(diag)
-    if ub + cnt <= ans:
+    # 답은 최대 개수를 찾는 것임
+    # 따라서 앞으로 더 높을 수 있는 개수인 dp[diag]에 지금까지 찾은 cnt를 더했을 때
+    # ans 이하이면 가지치기를 한다
+    if dp[diag] + cnt <= ans:
         return
 
     if diag < n:
