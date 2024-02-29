255534c34b6a25f47ec309ab78b1e0b18cf10852
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-02-29 15:56:10 +0900

    [Gold III] Title: 소문난 칠공주, Time: 1600 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -3,11 +3,13 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
+# 25C7 = 60720이니까 조합 이용해서 탐색해도 됨. 조합 쓰라고 범위 작게줬나?
+
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 arr = [list(input().strip()) for _ in range(5)]
-dasoms = set()
+dasoms = []  # 다솜이네
 
 
 def dfs(depth, start, yCount):
@@ -26,21 +28,25 @@ def dfs(depth, start, yCount):
         x = i // 5
         y = i % 5
 
-        dasoms.add((x, y))
+        dasoms.append([x, y])
 
         if arr[x][y] == "Y":
             dfs(depth + 1, i + 1, yCount + 1)
         else:
             dfs(depth + 1, i + 1, yCount)
 
-        dasoms.remove((x, y))  # 백트래킹
+        dasoms.pop()  # 백트래킹
 
 
 def checkPrincess():
-    visited = [[False] * 5 for _ in range(5)]
+    visited = [[True] * 5 for _ in range(5)]
     q = deque();
-    q.append(next(iter(dasoms)))
-    visited[next(iter(dasoms))[0]][next(iter(dasoms))[1]] = True
+    q.append(dasoms[0])
+
+    for x, y in dasoms:
+        visited[x][y] = False
+
+    visited[dasoms[0][0]][dasoms[0][1]] = True
     cnt = 1
 
     while len(q) > 0:
@@ -56,12 +62,9 @@ def checkPrincess():
             if visited[nx][ny]:  # 재방문 X
                 continue;
 
-            if (nx, ny) not in dasoms:  # 집합에 없으면 탐색할 필요 없음
-                continue;
-
             cnt += 1
             visited[nx][ny] = True
-            q.append((nx, ny))
+            q.append([nx, ny])
 
     return cnt == 7
 
