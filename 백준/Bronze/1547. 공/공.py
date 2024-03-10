d8575334fe834cf209f131e29c637f91c587d665
HotFried <121084350+bbamjoong@users.noreply.github.com>
2024-03-10 23:54:31 +0900

    [Bronze III] Title: 공, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+cups = [1,2,3]
+for _ in range(n):
+    x, y = map(int, input().split())
+    
+    xi = cups.index(x)
+    yi = cups.index(y)
+    
+    cups[xi], cups[yi] = cups[yi], cups[xi]
+    
+print(cups[0])
