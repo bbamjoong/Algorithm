984933f9c4abff8005e64e995ecdd9d47152afa9
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-03-12 16:24:37 +0900

    [Gold IV] Title: 사촌, Time: 432 ms, Memory: 113236 KB -BaekjoonHub

--- 백준/Gold/9489. 사촌/사촌.py
+++ 백준/Gold/9489. 사촌/사촌.py
@@ -9,7 +9,6 @@ while True:
 
     arr = [-1] + list(map(int, input().split()))
     parent = [0] * (n + 1)
-    parent[0] = -1  # 루트 노드는 parent가 없음
 
     target = 0
     idx = -1
@@ -17,14 +16,16 @@ while True:
         if arr[i] == k:
             target = i
 
-        if arr[i] > arr[i - 1] + 1:
+        if arr[i] > arr[i - 1] + 1:  # 2이상 차이가 나면.
+            # 루트노드의 경우 최소 1임. 그렇다면 0번째 값은 -1 이하로 배치해야함.
             idx += 1
 
         parent[i] = idx
-
+        
     ans = 0
-    for i in range(1, n+1):
-        if parent[i] != parent[target] and parent[parent[i]] == parent[parent[target]]:
-            ans += 1
+    for i in range(1, n + 1):
+        if parent[parent[target]] < parent[i]:
+            if parent[i] != parent[target] and parent[parent[i]] == parent[parent[target]]:
+                ans += 1
 
     print(ans)
