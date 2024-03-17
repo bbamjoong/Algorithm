eab722d4f5bf74ae3b0246720abdbe475c6d04a9
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-03-17 15:55:07 +0900

    [Silver IV] Title: 수열 정렬, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+t = list(map(int, input().split()))
+s_li = sorted(t)
+li = []
+
+for i in range(n):
+    idx = s_li.index(t[i])
+    li.append(idx)
+    s_li[idx] = -1
+print(*li)
