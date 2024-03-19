d9c7b994d69678887523989e96e52c56f447fb09
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-03-19 11:02:53 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 388 ms, Memory: 59244 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
@@ -15,29 +15,27 @@ visited = [False] * (n + 1)
 
 
 def dfs(node, cnt):
-    global visited, visit_cnt, res
-
+    global visited, visit_cnt, node_cnt, edge_cnt
+    node_cnt += 1  # 그래프 노드 개수 추가
+    edge_cnt += len(li[node])  # 그래프 엣지 개수 추가
     for next in li[node]:
         if not visited[next]:
             visited[next] = True
             visit_cnt += 1
-            res.append(next);
+
             dfs(next, cnt + 1)
 
 
 def check_cycle():
-    global res, li, tree_cnt
+    global node_cnt, edge_cnt, cut_cnt, tree_cnt
 
-    node_cnt = len(res)  # 노드의 개수
-    edge_cnt = 0
-    for i in res:
-        edge_cnt += len(li[i])
-    edge_cnt //= 2
+    edge_cnt //= 2  # 양방향 간선으로 저장했으니까 // 2
 
     if node_cnt - edge_cnt != 1:  # 사이클이 있다면
-        tree_cnt += edge_cnt - node_cnt + 1
+        cut_cnt += edge_cnt - node_cnt + 1
 
 
+cut_cnt = 0  # 사이클 끊은 횟수
 tree_cnt = 0  # 트리의 개수
 for i in range(1, n + 1):
     if not visited[i]:
@@ -45,12 +43,13 @@ for i in range(1, n + 1):
         visit_cnt += 1  # 방문한 노드 개수++
         tree_cnt += 1  # 트리의 개수++
 
-        res = [i]
-        dfs(i, 1)
+        node_cnt = 0
+        edge_cnt = 0
 
+        dfs(i, 1)
         check_cycle();
 
         if visit_cnt == n:
             break
 
-print(tree_cnt - 1)
+print(cut_cnt + tree_cnt - 1)
