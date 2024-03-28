84df1574bf72bc68886711b3d874f996b0b3632b
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-03-29 00:28:27 +0900

    [Platinum IV] Title: Wall construction, Time: 104 ms, Memory: 12312 KB -BaekjoonHub

---
+++ 백준/Platinum/10903. Wall construction/Wall construction.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int n;
+    static int l;
+    static List<Point> points;
+    static Point root;
+    static Stack<Point> stack;
+    static double ans;
+
+    static class Point implements Comparable<Point> {
+        long x;
+        long y;
+
+        Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        // 각도에 따라 정렬할 것
+        @Override
+        public int compareTo(Point p) {
+            long result = ccw(root, this, p);
+            if (result > 0) { // 반시계 -> 오름차순
+                return -1;
+            } else if (result < 0) { // 시계방향 -> 내림차순
+                return 1;
+            } else if (dist(root, this) < dist(root, p)) { // 일직선에 위치할 때 다음 점이 더 멀면 -> 오름차순
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    static void setInputs() throws IOException {
+        stack = new Stack<>();
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+
+        points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points.add(new Point(x, y));
+        }
+    }
+
+    static void grahamScan() {
+        findRoot();
+        Collections.sort(points);
+
+        stack.add(root);
+
+        for (int i = 1; i < points.size(); i++) {
+            // 시계방향이거나, 일자 방향이 나오면 pop
+            while (stack.size() > 1
+                    && ccw(stack.get(stack.size() - 2), stack.get(stack.size() - 1), points.get(i)) <= 0) {
+                stack.pop();
+            }
+            stack.add(points.get(i));
+        }
+    }
+
+    // y가 가장 작고, x가 가장 작은 점
+    static void findRoot() {
+        root = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+        for (Point point : points) {
+            if (point.y < root.y) {
+                root = point;
+            } else if (point.y == root.y) { // y가 같다면 x가 더 작은 점이 root
+                if (point.x < root.x) {
+                    root = point;
+                }
+            }
+        }
+    }
+
+    static long ccw(Point p1, Point p2, Point p3) {
+        return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+    }
+
+    static double dist(Point p1, Point p2) {
+        return Math.sqrt((Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
+    }
+
+    static void calculateAnswer() {
+        ans += 2 * Math.PI * l; // 곡선 부분의 합은 원의 지름이다.
+
+        stack.add(stack.get(0));
+        for (int i = 0; i < stack.size() - 1; i++) {
+            ans += dist(stack.get(i), stack.get(i + 1));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        setInputs();
+        grahamScan();
+        calculateAnswer();
+        System.out.println(ans);
+    }
+}
