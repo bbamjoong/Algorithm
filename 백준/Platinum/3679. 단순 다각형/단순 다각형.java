e34593aaa99ca8a3612b153c907225fe8510383a
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-03-29 00:14:50 +0900

    [Platinum IV] Title: 단순 다각형, Time: 768 ms, Memory: 34728 KB -BaekjoonHub

--- 백준/Platinum/3679. 단순 다각형/단순 다각형.java
+++ 백준/Platinum/3679. 단순 다각형/단순 다각형.java
@@ -1,83 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static List<Point> points;
+
     static class Point implements Comparable<Point> {
-        long x, y, idx;
+        long x;
+        long y;
+        int idx;
 
-        public Point(long x, long y, long idx) {
+        public Point(long x, long y, int idx) {
             this.x = x;
             this.y = y;
             this.idx = idx;
         }
 
         @Override
-        public int compareTo(Point other) {
-            if (this.x != other.x) {
-                return Long.compare(this.x, other.x);
+        public int compareTo(Point p) {
+            if (this.x != p.x) {
+                return Long.compare(this.x, p.x);
             } else {
-                return Long.compare(this.y, other.y);
+                return Long.compare(this.y, p.y);
             }
         }
     }
 
-    static int ccw(Point a, Point b, Point c) {
-        long res = a.x * b.y + b.x * c.y + c.x * a.y;
-        res -= b.x * a.y + c.x * b.y + a.x * c.y;
-        if (res > 0) {
+    static int ccw(Point p1, Point p2, Point p3) {
+        long res = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+
+        if (res > 0) { // 반시계
             return 1;
         }
-        if (res < 0) {
+        if (res < 0) { // 시계
             return -1;
         }
-        return 0;
+        return 0; // 일직선
     }
 
-    static long dist(Point a, Point b) {
-        long dx = a.x - b.x;
-        long dy = a.y - b.y;
-        return dx * dx + dy * dy;
+    static long dist(Point p1, Point p2) {
+        return (long) (Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        while (t-- > 0) {
-            int n = scanner.nextInt();
-            List<Point> v = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                long x = scanner.nextLong();
-                long y = scanner.nextLong();
-                v.add(new Point(x, y, i));
+    public static void main(String[] args) throws Exception {
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < t; tc++) {
+            st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            points = new ArrayList<>();
+
+            int idx = 0;
+            while (st.hasMoreTokens()) {
+                long x = Integer.parseInt(st.nextToken());
+                long y = Integer.parseInt(st.nextToken());
+                points.add(new Point(x, y, idx++));
             }
 
-            Point minPoint = Collections.min(v);
-            Collections.swap(v, 0, v.indexOf(minPoint));
+            Point root = Collections.min(points);
+            Collections.swap(points, 0, points.indexOf(root));
 
-            v.subList(1, v.size()).sort((a, b) -> {
-                int cw = ccw(v.get(0), a, b);
-                if (cw != 0) {
-                    return cw > 0 ? 1 : -1;
+            points.subList(1, points.size()).sort((a, b) -> {
+                int result = ccw(points.get(0), a, b);
+                if (result != 0) {
+                    if (result > 0) {
+                        return 1;
+                    }
+                    return -1;
                 }
-                return Long.compare(dist(v.get(0), a), dist(v.get(0), b));
+                return Long.compare(dist(points.get(0), a), dist(points.get(0), b));
             });
 
-            int pt = v.size() - 1;
-            for (int i = v.size() - 1; i >= 1; i--) {
-                if (ccw(v.get(0), v.get(pt), v.get(pt - 1)) == 0) {
-                    pt--;
+            int right = points.size() - 1;
+            for (int i = points.size() - 1; i >= 1; i--) {
+                if (ccw(root, points.get(right), points.get(right - 1)) == 0) {
+                    right--;
                 } else {
                     break;
                 }
             }
 
-            Collections.reverse(v.subList(pt, v.size()));
-            for (Point i : v) {
+            Collections.reverse(points.subList(right, points.size()));
+            for (Point i : points) {
                 System.out.print(i.idx + " ");
             }
             System.out.println();
         }
+
     }
 }
