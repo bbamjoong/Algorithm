9655f9864e175cc9790b6dc91debd1b9812aa50c
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-04-15 13:33:50 +0900

    [Gold V] Title: 입국심사, Time: 228 ms, Memory: 111056 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -4,7 +4,8 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
-start, end = min(arr), max(arr) * m
+# 걸리는 시간을 매개변수로 이용.
+start, end = min(arr), min(arr) * m
 
 while start <= end:
     total = 0
