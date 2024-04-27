fa5f6d7786eb40eba505fdff559b537c10e4d3df
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-04-27 20:37:48 +0900

    [Silver II] Title: 이항 계수 2, Time: 232 ms, Memory: 53648 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [[1] * (n + 1) for _ in range(n + 1)]
+
+# nCr = nCr-1 + n-1Cr-1
+for i in range(2, n+1) :
+    for j in range(1, i) :
+        dp[i][j]=(dp[i - 1][j - 1] + dp[i - 1][j]) % 10007
+
+print(dp[n][k])
