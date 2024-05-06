5128b9fd34fc2820f823d01717ca28159bcf52d4
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-05-06 23:52:26 +0900

    [Bronze I] Title: 암호 키, Time: 208 ms, Memory: 127068 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.py
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -1,12 +1,12 @@
-def prime(n):
-    li = [0, 0] + [1] * n
-    for i in range(2, n + 1):
-        if li[i]:
-            for j in range(2 * i, n + 1, i):
-                li[j] = 0
-    return li
+import sys
+input = sys.stdin.readline
 
-li = prime(1000000)
+n = 1000000
+li = [0, 0] + [1] * n
+for i in range(2, n + 1):
+    if li[i]:
+        for j in range(2 * i, n + 1, i):
+            li[j] = 0
 
 for _ in range(int(input())):
     n = int(input())
