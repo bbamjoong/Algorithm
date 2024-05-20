1453d5341de60b76d633e4cd8eb4d80eb86c8614
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-05-20 12:11:08 +0900

    [Silver IV] Title: 제곱근, Time: 52 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+start = 1
+end = n
+
+while start <= end:
+    mid = (start + end) // 2
+    if mid ** 2 >= n:
+        end = mid - 1
+        
+    elif mid ** 2 < n:
+        start = mid + 1
+
+print(start)
