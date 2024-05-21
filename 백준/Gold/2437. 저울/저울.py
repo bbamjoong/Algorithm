7c2a37fcaf77ae768070a4ef427001cb8665a3fc
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-05-21 10:12:10 +0900

    [Gold II] Title: 저울, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -6,7 +6,7 @@ li = list(map(int, input().split()))
 li.sort()
 
 # 정렬 -> 무게추를 하나씩 더하면서 범위를 지정해줌.
-ans = 1 # 답의 최소 값은 1임.
+value = 0 # 난 지금 0까지 탐색 가능함
 
 # 만약 내가 1~5까지 탐색가능함.
 # 그런데 다음 추가 3이라고 하면
@@ -15,9 +15,9 @@ ans = 1 # 답의 최소 값은 1임.
 # 그런데 다음 추가 6이라고 하면 가능하겠지.
 # 하지만 다음 추가 7이라고 하면 불가능하다.
 for num in li:
-    if ans < num:
+    if value < num - 1:
         break
 
-    ans += num
+    value += num
 
-print(ans)
+print(value + 1)
