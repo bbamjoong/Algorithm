529ca8e824a26041724f334befc7fdec43400438
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-07-05 09:50:41 +0900

    [Gold V] Title: 물병, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-def count_ones(x):
+def count_bit(x):
     count = 0
     while x > 0:
         count += x & 1
@@ -13,14 +13,14 @@ def count_ones(x):
 
 ans = 0
 while True:
-    cnt = count_ones(n)
+    cnt = count_bit(n)
 
     if cnt > k:
         # 가장 낮은 1의 비트를 찾는다.
-        lowest_one_bit = n & -n
+        lowest_bit = n & -n
         # 이 비트를 더하여 1의 개수를 줄인다.
-        n += lowest_one_bit
-        ans += lowest_one_bit
+        n += lowest_bit
+        ans += lowest_bit
     else:
         break
 
