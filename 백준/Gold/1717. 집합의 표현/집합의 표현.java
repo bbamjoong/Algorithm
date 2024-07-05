fb7fe7fc04f8dd4feec0efcb1a40bae1276a65b7
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-07-05 17:23:26 +0900

    [Gold V] Title: 집합의 표현, Time: 168 ms, Memory: 24280 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,57 +2,96 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
     static int n;
-    static int[] parent;
+    static int m;
+    static int[] parent; // 부모 노드 저장
 
-    public static int find(int a) {
-        if (a == parent[a]) {
-            return a;
+    static int find(int x) {
+        if (parent[x] == x) {
+            return x;
         }
-
-        return parent[a] = find(parent[a]);
+        return parent[x] = find(parent[x]); // find를 할 때 parent 값을 업데이트 해주기 때문에 속도가 더 빨라짐
     }
 
-    public static void union(int a, int b) {
-        if(a < b)
-            parent[find(b)] = find(a);
-        else
-            parent[find(a)] = find(b);
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a < b) {
+            parent[b] = a;
+            return;
+        }
+        parent[a] = b;
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = nextInt();
+        m = nextInt();
 
         parent = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < n + 1; i++) { // 자기 자신을 부모로 갖는다.
             parent[i] = i;
         }
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            String mode = st.nextToken();
-            int first = Integer.parseInt(st.nextToken());
-            int second = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) { // m번 연산 실행
+            int command = nextInt();
+            int a = nextInt();
+            int b = nextInt();
 
-            if (mode.equals("0")) {
-                union(first, second);
-            } else {
-                int a = find(first);
-                int b = find(second);
-                if (a == b) {
-                    sb.append("YES").append("\n");
-                } else {
-                    sb.append("NO").append("\n");
-                }
+            if (command == 0) {
+                union(a, b);
+                continue;
+            }
+
+            if (find(a) == find(b)) { // 부모가 같으면
+                sb.append("yes").append("\n");
+            } else { // 부모가 다르면
+                sb.append("no").append("\n");
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+    static final int SIZE = 1 << 13;
+    static byte[] buffer = new byte[SIZE];
+    static int index, size;
+
+    static int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        while ((c = read()) <= 32)
+            ;
+        boolean neg = c == '-' ? true : false;
+        if (neg) {
+            c = read();
+        }
+        do {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        while (isNumber(c = read()));
+        if (neg) {
+            return -n;
+        }
+        return n;
+    }
+
+    static boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    static byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) {
+                buffer[0] = -1;
             }
         }
-
-        System.out.println(sb);
+        return buffer[index++];
     }
 }
