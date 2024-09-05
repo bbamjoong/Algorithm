a8a3f17b61439d6de432e9d2b49071a9f1a60fa1
B.J Kim <121084350+bbamjoong@users.noreply.github.com>
2024-09-05 09:09:38 +0900

    [Bronze V] Title: 세제곱의 합, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+sum = 0
+cube_sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+    cube_sum += i ** 3
+
+print(sum)
+print(sum ** 2)
+print(cube_sum)
