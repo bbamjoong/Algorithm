b620dd4a55c04f4077c7c5780c4c429e9d614e5c
B.J Kim <rlaqjawnd625@naver.com>
2025-02-05 11:08:06 +0900

    [Silver III] Title: N과 M (7), Time: 552 ms, Memory: 220512 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] values;
+    static int[] res;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        values = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(values);
+
+        res = new int[m];
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+
+        if (depth == m) {
+            for (int i : res) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            res[depth] = values[i];
+            dfs(depth + 1);
+        }
+    }
+}
