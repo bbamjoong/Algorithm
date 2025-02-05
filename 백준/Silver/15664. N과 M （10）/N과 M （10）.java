f84cf9d8a4425ed33ac572597708f96159ceb30e
B.J Kim <rlaqjawnd625@naver.com>
2025-02-05 11:46:46 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] values, res;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        values = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(values);
+
+        visited = new boolean[n + 1];
+        res = new int[m];
+
+        dfs(0, 1);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int index) {
+        if (depth == m) {
+            for (int i : res) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int lastUsed = -1;  // 이전에 사용한 값 저장
+        for (int i = index; i < n + 1; i++) {
+            if (!visited[i] && values[i] != lastUsed) {
+                res[depth] = values[i];
+                lastUsed = values[i];
+
+                visited[i] = true;
+                dfs(depth + 1, i);
+                visited[i] = false;
+            }
+        }
+    }
+}
