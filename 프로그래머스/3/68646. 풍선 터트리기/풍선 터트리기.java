28b61991389775d369b5a0df93c3576f15a27bef
B.J Kim <rlaqjawnd625@naver.com>
2025-04-22 17:04:39 +0900

    [level 3] Title: 풍선 터트리기, Time: 264.63 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,34 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] a) {
-        int n = a.length;
-        if (n <= 2) {
-            return n;  // 1개나 2개면 무조건 모두 생존 가능
-        }
-
-        int[] beforeMin = new int[n];
-        int[] afterMin = new int[n];
-
-        // 1) beforeMin 채우기
-        beforeMin[0] = a[0];
-        for (int i = 1; i < n; i++) {
-            beforeMin[i] = Math.min(beforeMin[i-1], a[i]);
-        }
-
-        // 2) afterMin 채우기
-        afterMin[n-1] = a[n-1];
-        for (int i = n - 2; i >= 0; i--) {
-            afterMin[i] = Math.min(afterMin[i+1], a[i]);
-        }
-
-        // 3) 생존 가능 풍선 개수 카운트
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] > beforeMin[i] && a[i] > afterMin[i]) {
-                continue;
-            }
-            count++;
+        int answer = 0;
+        
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i = 0 ; i < a.length ; i++){
+            min1 = Math.min(min1, a[i]);
+            min2 = Math.min(min2, a[a.length -1 -i]);
+            
+            hs.add(min1);
+            hs.add(min2);
         }
         
-        return count;
+        return hs.size();
     }
 }
