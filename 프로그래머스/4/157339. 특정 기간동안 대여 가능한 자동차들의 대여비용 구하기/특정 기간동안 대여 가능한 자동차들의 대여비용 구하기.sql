365940629f7dd66fd84cc8fce6456ce3027e45cf
B.J Kim <rlaqjawnd625@naver.com>
2025-05-15 18:07:23 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,46 +1,14 @@
-SELECT C.CAR_ID as CAR_ID,
-       C.CAR_TYPE as CAR_TYPE,
-       ROUND(C.DAILY_FEE*30*(100-P.DISCOUNT_RATE)/100) AS FEE
-       
-    FROM CAR_RENTAL_COMPANY_CAR C
-            JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H ON C.CAR_ID = H.CAR_ID
-            JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P ON C.CAR_TYPE = P.CAR_TYPE
-            
-    WHERE C.CAR_ID NOT IN ( 
-        SELECT CAR_ID
-        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        WHERE END_DATE >= '2022-11-01' AND START_DATE <= '2022-12-01'
-        
-)
-    AND P.DURATION_TYPE like '30%'
-    
-GROUP BY C.CAR_ID
-
-    HAVING C.CAR_TYPE IN ('세단', 'SUV')
-        AND (FEE >= 500000 AND FEE < 2000000)
-    
-ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC
-
-
-# SELECT C.CAR_ID as CAR_ID,
-#        C.CAR_TYPE as CAR_TYPE,
-#        ROUND(C.DAILY_FEE*30*(100-P.DISCOUNT_RATE)/100) AS FEE
-       
-#     FROM CAR_RENTAL_COMPANY_CAR C
-#             JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H ON C.CAR_ID = H.CAR_ID
-#             JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P ON C.CAR_TYPE = P.CAR_TYPE
-            
-#     WHERE C.CAR_ID NOT IN ( 
-#         SELECT CAR_ID
-#         FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-#         WHERE END_DATE >= '2022-11-01' AND START_DATE <= '2022-12-01'
-        
-# )
-#     AND P.DURATION_TYPE like '30%'
-    
-# GROUP BY C.CAR_ID
-
-#     HAVING C.CAR_TYPE IN ('세단', 'SUV')
-#         AND (FEE >= 500000 AND FEE < 2000000)
-    
-# ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC
+select c.car_id, c.car_type, 
+    round(c.daily_fee * 30 * (100 - dp.discount_rate) / 100) as fee
+from car_rental_company_car c
+join car_rental_company_discount_plan dp
+on c.car_type = dp.car_type
+where c.car_id not in
+    (select car_id
+     from car_rental_company_rental_history
+     where end_date >= '2022-11-01' and start_date <= '2022-12-01'
+    )
+    and c.car_type in ('세단', 'suv')
+    and dp.duration_type like "30%"
+    and round(c.daily_fee * 30 * (100 - dp.discount_rate) / 100) >= 500000
+    and round(c.daily_fee * 30 * (100 - dp.discount_rate) / 100) <= 2000000
